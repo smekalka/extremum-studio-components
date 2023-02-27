62148376a73e44a2600af381e0553d22250e5186
VolkovskayaUliana <uliana.volkovskaya@smekalka.com>
2023-02-27 14:48:52 +0300

    chore: add grids component

--- src/modules/grids/utils/hooks/useGetGridsList.ts
+++ src/modules/grids/utils/hooks/useGetGridsList.ts
@@ -1,52 +1,52 @@
-import { useEffect, useState } from "react";
-import { GridsController } from "../../service/gridsController";
-import { HandlerDataFunc } from "../../types/functions";
-import { RowsData } from "../../types/grid";
+import {useEffect, useState} from "react";
+import {GridsController} from "../../service/gridsController";
+import {HandlerDataFunc} from "../../types/functions";
+import {RowsData} from "../../types/grid";
 
 export const useGetGridsList = (controller: GridsController<any>, vscode: any, dataHandler?: HandlerDataFunc) => {
-  const [gridsData, setGridsData] = useState<RowsData | undefined>();
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(false);
+    const [gridsData, setGridsData] = useState<RowsData | undefined>();
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(false);
 
-  const updateDataFromServe = () => {
+    const updateDataFromServe = () => {
 
-    setIsLoading(true);
-    controller.getAll().then(res => {
-      const finalData = dataHandler ? dataHandler(res) : res
+        setIsLoading(true);
+        controller.getAll().then(res => {
+            const finalData = dataHandler ? dataHandler(res) : res
 
 
-      vscode.setState({
-        init: finalData,
-        data: finalData,
-        ids: [],
-        changed: [],
-        newRows: [],
-        removedRows:[],
-      });
+            vscode.setState({
+                init: finalData,
+                data: finalData,
+                ids: [],
+                changed: [],
+                newRows: [],
+                removedRows: [],
+            });
 
-      setGridsData(finalData)
-      setIsLoading(false);
-    });
-  };
+            setGridsData(finalData)
+            setIsLoading(false);
+        }).catch(err => setError(err))
+    };
 
-  useEffect(() => {
-    const vscodeState = vscode.getState();
+    useEffect(() => {
+        const vscodeState = vscode.getState();
 
-    if (!vscodeState){
-      vscode.setState({})
-    }
+        if (!vscodeState) {
+            vscode.setState({})
+        }
 
-    const dataFromState = vscode.getState().data;
+        const dataFromState = vscode.getState().data;
 
-    if (!dataFromState || dataFromState.length === 0) {
-      updateDataFromServe();
-    } else {
-      setGridsData(dataFromState);
-    }
-  }, []);
-  useEffect(()=>{
-    console.log(gridsData,"GRIDSDATA")
-  },[gridsData])
+        if (!dataFromState || dataFromState.length === 0) {
+            updateDataFromServe();
+        } else {
+            setGridsData(dataFromState);
+        }
+    }, []);
+    useEffect(() => {
+        console.log(gridsData, "GRIDSDATA")
+    }, [gridsData])
 
-  return { gridsData, setGridsData, updateDataFromServe, error, isLoading };
+    return {gridsData, setGridsData, updateDataFromServe, error, isLoading};
 };
