0817b3f75ef579d0585d003482a8cc96e786c0fa
VolkovskayaUliana <uliana.volkovskaya@smekalka.com>
2023-02-28 18:01:59 +0300

    fix: fix imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
 import Grids from "./modules/grids/Grids";
-
-
+const GridsController = require("./modules/grids/service")
 // Please do not use types off of a default export module or else Storybook Docs will suffer.
 // see: https://github.com/storybookjs/storybook/issues/9556
 
 
-export {Grids}
+export {Grids, GridsController}
